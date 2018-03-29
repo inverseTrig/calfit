429f5bc5c4344b96a26152767b5cc40ffb2b74b4
HeeChan Kang <heech.kang@gmail.com>
2018-03-28 20:00:30 -0500

    Added essential classes and files we will be needing.

--- app/src/main/java/edu/augsburg/calfit/MainActivity.java
+++ app/src/main/java/edu/augsburg/calfit/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.augsburg.calfit;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,6 +41,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, new DashboardFragment()).commit();
+
     }
 
     @Override
@@ -79,14 +84,17 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager = getFragmentManager();
 
         if (id == R.id.nav_dashboard) {
-            // Handle the camera action
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new DashboardFragment()).commit();
         } else if (id == R.id.nav_grocery) {
-
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new GroceryFragment()).commit();
         } else if (id == R.id.nav_exercise) {
-
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new ExerciseFragment()).commit();
         } else if (id == R.id.nav_recipe) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new RecipesFragment()).commit();
+        } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_contact_us) {
 
